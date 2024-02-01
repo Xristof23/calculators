9143185dc00b5117499adfe5f873328b62a5831c
Christof Röhl <userpattern@gmail.com>
2024-02-01 13:02:52 +0100

    started with multiplication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,92 @@
 import React from "react";
 import "./styles.css";
+const firstInput = document.getElementById("firstfactor");
+const firstFactor = "123.456";
+const multiplyButton = document.querySelector("[data-js='multiply-button']");
+
+function ShowInput() {
+  firstInput.addEventListener("input", () => {
+    const firstFactor = firstInput.value;
+    return <p>{firstFactor}</p>;
+  });
+}
+
+
+function DeletePoint(a) {
+  const withoutPoint = getIntegerString(a) + getFractionString(a);
+  console.log(withoutPoint);
+  return (
+    <article>
+      <p>{withoutPoint}</p>
+    </article>
+  );
+}
 
 export default function App() {
-  return <div>Say hello...</div>;
+  return (
+    <main>
+      <article>
+        <h1>Calculate in writing</h1>
+        <p>
+          Have you ever thought about the complexity of some algorithms you
+          already know? <br></br>
+          All the way back to primary school ...{" "}
+        </p>
+      </article>
+      <div className="calculator">
+        <h2>Multiply</h2>
+        <div className="inputs">
+          <label htmlFor="firstfactor">First factor</label>
+          <input id="firstfactor"></input>
+          {/* <ShowInput /> */}
+          <label htmlFor="secondfactor ">Second factor</label>
+          <input id="secondfactor"></input>
+          <button className="calculate-button" data-js="multiply-button">
+            =
+          </button>
+
+          <p>Result</p>
+          <p className="inputs" data-js="product-result"></p>
+        </div>
+      </div>
+      <StringToWords />
+    </main>
+  );
+}
+
+// Für später
+
+const testString = "1234567";
+function StringToArray() {
+  const firstArray = testString.split("");
+  // console.log(firstArray);
+  return (
+    <article>
+      <p>
+        {firstArray[0]}
+        {firstArray[1]}
+        {firstArray[2]}
+      </p>
+    </article>
+  );
+}
+
+const testText = "Hallo wie geht es dir?";
+function StringToWords() {
+  const testArray = testText.split(" ");
+  // console.log(firstArray);
+  return (
+    <article>
+      <p>
+        {testArray[0]}
+        {}
+        {testArray[1]}
+        {}
+        {testArray[2]}
+        {}
+        {testArray[3]}
+        {}
+      </p>
+    </article>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,51 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+// testdrive
+function getIntegerString(a) {
+  if (a.includes(".")) {
+    return a.split(".")[0];
+  }
+  return a;
+}
+function getFractionString(a) {
+  if (a.includes(".")) {
+    return a.split(".")[1];
+  }
+  return "";
+}
+const testDrive = "4.52";
+const y = "3.1";
+function deletePoint(a) {
+  const withoutPoint = getIntegerString(a) + getFractionString(a);
+  return withoutPoint;
+  /* (
+    <article>
+      <p>{withoutPoint}</p>
+    </article>
+  ) */
+}
+const multiplication = (a, b) => a * b;
+
+console.log("Dummer Hans: " + multiplication(testDrive, y));
+
+const addition = (a, b) => a + b;
+
+function correctMultiplication(a, b) {
+  const aDecimalPlaces = Number(getFractionString(a).length);
+  const bDecimalPlaces = Number(getFractionString(b).length);
+  const decimalSum = addition(aDecimalPlaces, bDecimalPlaces);
+  const pointlessProduct = multiplication(deletePoint(a), deletePoint(b));
+  const decimalSumPlaces = Number(pointlessProduct.toString().length);
+  const integerPlaces = decimalSumPlaces - Number(decimalSum);
+  const result =
+    pointlessProduct.toString().substring(0, integerPlaces) +
+    "." +
+    pointlessProduct.toString().substring(integerPlaces, decimalSumPlaces);
+  return result;
+}
+console.log("Schlauer Kerl: " + correctMultiplication(testDrive, y));
+
 root.render(
   <StrictMode>
     <App />
